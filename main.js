1f7c06ad600519a4d76f17de8efef3bf4cfdaf50
Wellington-m <wellingtonmachioni@gmail.com>
2025-03-04 00:03:39 -0300

    feat: add activity and calculate average

--- main.js
+++ main.js
@@ -1,4 +1,46 @@
-document.querySelectorAll("input[type='number']").forEach((input) => {
-    const placeholderLength = input.placeholder.length;
-    input.style.width = `${placeholderLength + 3}ch`;
+const activityForm = document.getElementById("activityForm");
+const happyEmoji = "src='./images/aprovado.png' alt='Emoji festejando'";
+const sadEmoji = 'src="./images/reprovado.png" alt="Emoji triste"';
+const activities = [];
+
+let linhas = "";
+let totalValue = 0;
+let divisor = 0;
+
+activityForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const activityName = document.getElementById("activityName");
+    const activityValue = document.getElementById("activityValue");
+
+    let linha = `<tr>`;
+    linha += `<td>${activityName.value}</td>`;
+    linha += `<td>${activityValue.value}</td>`;
+    linha += `<td>`;
+    linha += `<img ${activityValue.value >= 7 ? happyEmoji : sadEmoji}/>`;
+    linha += `</td>`;
+    linha += `</tr>`;
+
+    linhas += linha;
+
+    const contentForm = document.querySelector("tbody");
+    contentForm.innerHTML = linhas;
+
+    activities.push({
+        activityName: activityName.value,
+        activityNote: parseFloat(activityValue.value),
+    });
+
+    const activityNotes = activities.map((value) => value.activityNote);
+    const average = activityNotes.length
+        ? activityNotes.reduce((acc, curr, _index, array) => (acc += curr), 0) /
+          activityNotes.length
+        : 0;
+
+    const averageElement = document.querySelector("tfoot tr").children[1];
+
+    averageElement.textContent = average;
+
+    activityName.value = "";
+    activityValue.value = "";
 });
