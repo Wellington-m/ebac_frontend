ae2e6d5c9659918c7f9a7227c26adef7f098c249
Wellington-m <wellingtonmachioni@gmail.com>
2025-03-14 16:04:37 -0300

    feat: implemented add items to the to-do list

---
+++ script.js
@@ -0,0 +1,35 @@
+$(document).ready(() => {
+    const priorityImages = {
+        high_priority_input: {
+            src: "./images/High_priority.svg",
+            alt: "Um polígono vermelho",
+        },
+        medium_priority_input: {
+            src: "./images/Medium_priority.svg",
+            alt: "Um polígono amarelo",
+        },
+        low_priority_input: {
+            src: "./images/Low_priority.svg",
+            alt: "Um polígono azul",
+        },
+    };
+
+    $("form").on("submit", (e) => {
+        e.preventDefault();
+        const todoValue = $("#todoValue").val();
+        const priority = $("input[name='priority']:checked").val();
+        const newItem = $("<li class='todo-item'></li>");
+        $(`<h1 class='todo-title'>
+            ${todoValue}
+            </h1>`).appendTo(newItem);
+
+        $(`<img
+                src=${priorityImages[priority].src}
+                alt=${priorityImages[priority].alt}
+            />`).appendTo(newItem);
+
+        $(newItem).appendTo("ul");
+
+        $("#todoValue").val("");
+    });
+});
