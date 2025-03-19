3e20f16c51dd8f9f59f7c2a6270d02aeeabfab72
Wellington-m <wellingtonmachioni@gmail.com>
2025-03-19 20:30:29 -0300

    feat: created validations to form

--- main.js
+++ main.js
@@ -4,4 +4,29 @@ $(document).ready(() => {
     $(".burger_menu").on("click", () => {
         $("nav").slideToggle();
     });
+
+    $("#telephone").mask("(00) 00000-0000");
+
+    $("form").validate({
+        rules: {
+            name: {
+                required: true,
+            },
+            telephone: {
+                required: true,
+            },
+            email: {
+                required: true,
+            },
+            vehicle: {
+                required: true,
+            },
+        },
+        messages: {
+            name: "Este campo é obrigatório",
+        },
+        submitHandler: (form) => {
+            console.log("Enviado");
+        },
+    });
 });
