779f85f67b5c342d08fc58502b39ac8d7b274d4d
Wellington <wellingtonmachioni@gmail.com>
2025-12-01 22:32:47 -0300

    chore(build): enable sourcemaps for Sass compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
 
 function compileHTML() {
   return gulp.src("./source/index.html").pipe(gulp.dest("build/"));
@@ -8,7 +9,9 @@ function compileHTML() {
 function compileSass() {
   return gulp
     .src("./source/styles/main.scss")
+    .pipe(sourcemaps.init())
     .pipe(sass({ style: "compressed" }).on("Error", sass.logError))
+    .pipe(sourcemaps.write("./maps"))
     .pipe(gulp.dest("./build/styles"));
 }
 
